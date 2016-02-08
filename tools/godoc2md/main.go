32c09f5b5cb4cbb0cf4140732042afb6e32e8fca
Raphael Simon <simon.raphael@gmail.com>
2016-02-07 22:31:08 -0800

    Add anchors to generated docs

---
+++ tools/godoc2md/main.go
@@ -0,0 +1,136 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// godoc2md converts godoc formatted package documentation into Markdown format.
+//
+//
+// Usage
+//
+//    godoc2md $PACKAGE > $GOPATH/src/$PACKAGE/README.md
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"go/build"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"text/template"
+
+	"golang.org/x/tools/godoc"
+	"golang.org/x/tools/godoc/vfs"
+)
+
+var (
+	verbose = flag.Bool("v", false, "verbose mode")
+
+	// file system roots
+	// TODO(gri) consider the invariant that goroot always end in '/'
+	goroot = flag.String("goroot", runtime.GOROOT(), "Go root directory")
+
+	// layout control
+	tabWidth       = flag.Int("tabwidth", 4, "tab width")
+	showTimestamps = flag.Bool("timestamps", false, "show timestamps with directory listings")
+	templateDir    = flag.String("templates", "", "directory containing alternate template files")
+	showPlayground = flag.Bool("play", false, "enable playground in web interface")
+	showExamples   = flag.Bool("ex", false, "show examples in command line mode")
+	declLinks      = flag.Bool("links", true, "link identifiers to their declarations")
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr,
+		"usage: godoc2md package [name ...]\n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+var (
+	pres *godoc.Presentation
+	fs   = vfs.NameSpace{}
+
+	funcs = map[string]interface{}{
+		"comment_md": comment_mdFunc,
+		"base":       path.Base,
+		"md":         mdFunc,
+		"pre":        preFunc,
+		"anchor":     anchorFunc,
+	}
+)
+
+const punchCardWidth = 80
+
+func comment_mdFunc(comment string) string {
+	var buf bytes.Buffer
+	ToMD(&buf, comment, nil)
+	return buf.String()
+}
+
+func mdFunc(text string) string {
+	text = strings.Replace(text, "*", "\\*", -1)
+	text = strings.Replace(text, "_", "\\_", -1)
+	return text
+}
+
+func preFunc(text string) string {
+	return "``` go\n" + text + "\n```"
+}
+
+func anchorFunc(elems ...string) string {
+	anchor := strings.Join(elems, "-")
+	return `<a name="` + strings.ToLower(anchor) + `"></a>`
+}
+
+func readTemplate(name, data string) *template.Template {
+	// be explicit with errors (for app engine use)
+	t, err := template.New(name).Funcs(pres.FuncMap()).Funcs(funcs).Parse(string(data))
+	if err != nil {
+		log.Fatal("readTemplate: ", err)
+	}
+	return t
+}
+
+func readTemplates(p *godoc.Presentation, html bool) {
+	p.PackageText = readTemplate("package.txt", pkgTemplate)
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	// Check usage
+	if flag.NArg() == 0 {
+		usage()
+	}
+
+	// use file system of underlying OS
+	fs.Bind("/", vfs.OS(*goroot), "/", vfs.BindReplace)
+
+	// Bind $GOPATH trees into Go root.
+	for _, p := range filepath.SplitList(build.Default.GOPATH) {
+		fs.Bind("/src/pkg", vfs.OS(p), "/src", vfs.BindAfter)
+	}
+
+	corpus := godoc.NewCorpus(fs)
+	corpus.Verbose = *verbose
+
+	pres = godoc.NewPresentation(corpus)
+	pres.TabWidth = *tabWidth
+	pres.ShowTimestamps = *showTimestamps
+	pres.ShowPlayground = *showPlayground
+	pres.ShowExamples = *showExamples
+	pres.DeclLinks = *declLinks
+	pres.SrcMode = false
+	pres.HTMLMode = false
+
+	readTemplates(pres, false)
+
+	if err := godoc.CommandLine(os.Stdout, fs, pres, flag.Args()); err != nil {
+		log.Print(err)
+	}
+}
---
+++ tools/godoc2md/template.go
@@ -0,0 +1,59 @@
+package main
+
+var pkgTemplate = `{{with .PDoc}}{{if $.IsMain}}
+# {{ base .ImportPath }}
+
+{{comment_md .Doc}}
+{{else}}
+# {{ .Name }}
+    import "{{.ImportPath}}"
+
+{{comment_md .Doc}}
+{{example_html $ ""}}
+
+{{with .Consts}}## Constants
+{{range .}}{{node $ .Decl | pre}}
+{{comment_md .Doc}}{{end}}{{end}}
+{{with .Vars}}## Variables
+{{range .}}{{node $ .Decl | pre}}
+{{comment_md .Doc}}{{end}}{{end}}
+{{range .Funcs}}{{$name_html := html .Name}}## func {{$name_html}}{{anchor $ .Name}}
+{{node $ .Decl | pre}}
+{{comment_md .Doc}}
+{{example_html $ .Name}}{{end}}
+{{range .Types}}{{$tname := .Name}}{{$tname_html := html .Name}}## type {{$tname_html}}{{anchor $ .Name}}
+{{node $ .Decl | pre}}
+{{comment_md .Doc}}
+
+{{range .Consts}}{{node $ .Decl | pre }}
+{{comment_md .Doc}}{{end}}
+
+{{range .Vars}}{{node $ .Decl | pre }}
+{{comment_md .Doc}}{{end}}
+
+{{example_html $ $tname}}
+
+{{range .Funcs}}{{$name_html := html .Name}}### func {{$name_html}}{{anchor $ $tname .Name}}
+{{node $ .Decl | pre}}
+{{comment_md .Doc}}
+{{example_html $ .Name}}{{end}}
+
+{{range .Methods}}{{$name_html := html .Name}}### func ({{md .Recv}}) {{$name_html}}{{anchor $ $tname .Name}}
+{{node $ .Decl | pre}}
+{{comment_md .Doc}}
+{{$name := printf "%s_%s" $tname .Name}}{{example_html $ $name}}
+{{end}}{{end}}{{end}}
+
+{{with $.Notes}}
+{{range $marker, $content := .}}
+## {{noteTitle $marker | html}}s
+<ul style="list-style: none; padding: 0;">
+{{range .}}
+<li><a href="{{posLink_url $ .}}">&#x261e;</a> {{html .Body}}</li>
+{{end}}
+</ul>
+{{end}}
+{{end}}
+{{end}}
+
+{{with .PAst}}{{node_html $ . false}}{{end}}`
